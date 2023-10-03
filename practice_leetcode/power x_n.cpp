8691d3cd16765a6dec8dc416652e38404940bdff
GUPTA-Vishu <118997257+GUPTA-Vishu@users.noreply.github.com>
2023-10-03 20:35:12 +0530

    power (x,n)

---
+++ practice_leetcode/power x_n.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+      if(n==0){
+        return 1;
+
+      }
+    long long ln=n;
+     if(ln%2==0){
+       
+       double temp;
+       if(ln<0){
+         x=1/x;
+         ln=-ln;
+         temp=myPow(x,ln/2);
+         return temp*temp;
+
+       }
+       else{
+         temp=myPow(x,ln/2);
+         return temp*temp;
+
+       }
+
+     }
+     else{
+       
+
+       if(ln<0){
+         x=1/x;
+         ln =-ln;
+
+         return  x*myPow(x,ln-1);
+
+       }
+       else{
+         return x*myPow(x,ln-1);
+
+       }
+     }
+
+        
+    }
+};
